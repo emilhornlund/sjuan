c2e5ecea5c559267d3da7b8eb8e39e785ad4dd2f
Emil Hörnlund <emil.hornlund@me.com>
2023-07-25 23:46:49 +0200

    Overhaul ActionDecider to prioritize actions and add corresponding tests
    This commit revamps the ActionDecider class to prioritize actions based on their type, rather than randomly choosing from the set of possible actions. The decision-making process now follows these steps:
    
    - Group actions by their type
    - Iterate over action types in order of their priority
    - For ActionType.PLAY_CARD and ActionType.GIVE_CARD, score the cards and select the best one
    - For other action types, if there are any actions of this type, return the first one
    
    The scoring system for cards is also introduced, which scores each card based on the current state of the game board. A lower score indicates a better card to play. The score is calculated based on the difference between the rank of the card and the nearest rank that can be played on the board.
    
    In addition, this commit adds comprehensive test cases for the ActionDecider class in the new test_action_decider.py file. These tests cover various scenarios and edge cases, ensuring that the decision-making process behaves as expected.

--- lib/action_decider.py
+++ lib/action_decider.py
@@ -1,32 +1,117 @@
 # lib/action_decider.py
 
-import random
-from typing import List
+from typing import Dict, List, Tuple
 
-
-from .action import Action
+from .action import Action, ActionType
+from .card import Card, Rank, Suit
 from .turn import Turn
 
 
 class ActionDecider:
     """
-    This class is used to decide the next action based on the current state of the game.
-
-    The decision is made by the static method `decide_action` which, in its current implementation, 
-    chooses an action randomly from the set of possible actions.
+    The ActionDecider class is responsible for deciding the best action to take based on the state of the game board and 
+    the available actions for the current turn.
     """
 
     @staticmethod
     def decide_action(board_matrix: List[List[bool]], turn: Turn) -> Action:
         """
-        Decide the next action based on the current state of the board and the possible actions.
+        Decide the best action to take given the state of the game board and the available actions for the current turn.
 
-        The current implementation randomly selects an action from the set of possible actions.
-
-        :param board_matrix: The current state of the board, represented as a 2D list of boolean values.
-        :param turn: The current turn, which contains the set of possible actions.
-        :return: The chosen action.
+        :param board_matrix: A 2D list where each sublist corresponds to a suit and each boolean in the sublist 
+                             indicates whether the card of that suit and rank has been played (True if it has, False otherwise).
+        :param turn: The current turn, which includes the available actions.
+        :return: The best action to take.
         """
 
-        # Randomly choose an action from the set of possible actions
-        return random.choice(list(turn.actions))
+        # Define the priority for each action type
+        action_priorities = {
+            ActionType.PLAY_ALL_CARDS: 1,
+            ActionType.GIVE_CARD: 2,
+            ActionType.PLAY_CARD: 3,
+            ActionType.TAKE_CARD: 4,
+            ActionType.PASS_TURN: 5
+        }
+
+        # Group actions by their type
+        actions_by_type = {action_type: [] for action_type in ActionType}
+        for action in turn.actions:
+            actions_by_type[action.type].append(action)
+
+        # Iterate over action types in order of their priority
+        for action_type in sorted(action_priorities, key=action_priorities.get):
+            if action_type == ActionType.PLAY_CARD:
+                # Analyze the board and score the cards for this action type
+                board_state = ActionDecider._ActionDecider__analyze_board(
+                    board_matrix)
+                scores = ActionDecider._ActionDecider__score_cards(
+                    [action.card for action in actions_by_type[action_type]], board_state)
+                if scores:
+                    # Choose the card with the lowest score (i.e., the best card to play)
+                    card_choice = min(scores, key=scores.get)
+                    # Return the action that corresponds to the chosen card
+                    return [action for action in actions_by_type[action_type] if action.card == card_choice][0]
+            elif action_type == ActionType.GIVE_CARD:
+                # Analyze the board and score the cards for this action type
+                board_state = ActionDecider._ActionDecider__analyze_board(
+                    board_matrix)
+                scores = ActionDecider._ActionDecider__score_cards(
+                    [action.card for action in actions_by_type[action_type]], board_state)
+                if scores:
+                    # Choose the card with the highest score (i.e., the best card to give)
+                    card_choice = max(scores, key=scores.get)
+                    # Return the action that corresponds to the chosen card
+                    return [action for action in actions_by_type[action_type] if action.card == card_choice][0]
+            elif actions_by_type[action_type]:
+                # If there are any actions of this type, return the first one
+                return actions_by_type[action_type][0]
+
+        # If no decision could be made, raise an exception
+        raise ValueError("No valid actions available for the current turn.")
+
+    @staticmethod
+    def __analyze_board(board_matrix: List[List[bool]]) -> Dict[Suit, Tuple[Rank, Rank]]:
+        """
+        Analyze the game board and determine the minimum and maximum card rank for each suit.
+
+        :param board_matrix: A 2D list where each sublist corresponds to a suit and each boolean in the sublist 
+                                indicates whether the card of that suit and rank has been played.
+        :return: A dictionary mapping each suit to a tuple of the minimum and maximum card rank for that suit.
+        """
+
+        board_state = {}
+        for suit, suit_cards in enumerate(board_matrix):
+            played_cards = [Rank(rank+1)
+                            for rank, played in enumerate(suit_cards) if played]
+            if played_cards:
+                min_card = min(played_cards)
+                max_card = max(played_cards)
+            else:
+                # If no cards of this suit have been played, assume the minimum and maximum card rank are both SEVEN
+                min_card = max_card = Rank.SEVEN
+            board_state[Suit(suit+1)] = (min_card, max_card)
+        return board_state
+
+    @staticmethod
+    def __score_cards(cards: List[Card], board_state: Dict[Suit, Tuple[Rank, Rank]]) -> Dict[Card, int]:
+        """
+        Score each card based on the current state of the game board. A lower score indicates a better card to play.
+        The score is calculated based on the difference between the rank of the card and the nearest rank that can be played on the board.
+
+        :param cards: A list of cards to score.
+        :param board_state: A dictionary mapping each suit to a tuple of the minimum and maximum card rank for that suit.
+        :return: A dictionary mapping each card to its score.
+        """
+
+        scores = {}
+        for card in cards:
+            min_card, max_card = board_state[card.suit]
+            if card.rank > max_card:
+                score = card.rank.value - max_card.value
+            elif card.rank < min_card:
+                score = min_card.value - card.rank.value
+            else:
+                # If the card can be played immediately, its score is 0
+                score = 0
+            scores[card] = score
+        return scores
---
+++ tests/test_action_decider.py
@@ -0,0 +1,298 @@
+import unittest
+from lib.action import Action, ActionType
+
+from lib.action_decider import ActionDecider
+from lib.card import Card, Rank, Suit
+from lib.player import Player, PlayerType
+from lib.turn import Turn
+
+
+class TestActionDecider(unittest.TestCase):
+    def test_decide_action_play_all_cards(self):
+        # Create a board state where no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+
+        # Create actions for the turn
+        actions = {Action(ActionType.PLAY_ALL_CARDS)}
+
+        # Create a turn with the actions
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # The expected action is to play all cards, because it's the only action available
+        expected_action = Action(ActionType.PLAY_ALL_CARDS)
+
+        # Call decide_action and check that it returns the expected action
+        actual_action = ActionDecider.decide_action(
+            board_matrix=board_matrix, turn=turn)
+        self.assertEqual(actual_action, expected_action)
+
+    def test_decide_action_give_card(self):
+        # Create a board state where no cards have been played yet
+        board_matrix = [
+            [False, False, False, False, False, False, True, False,
+                False, False, False, False, False],  # Hearts
+            [False]*13,  # Diamonds
+            [False]*13,  # Clubs
+            [False]*13,  # Spades
+        ]
+
+        # Create actions for the turn
+        actions = {Action(ActionType.GIVE_CARD, Card(Suit.HEARTS, Rank.ACE)), Action(
+            ActionType.GIVE_CARD, Card(Suit.HEARTS, Rank.FIVE))}
+
+        # Create a turn with the actions
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # The expected action is to give a card, because it's the only action available
+        expected_action = Action(ActionType.GIVE_CARD,
+                                 Card(Suit.HEARTS, Rank.ACE))
+
+        # Call decide_action and check that it returns the expected action
+        actual_action = ActionDecider.decide_action(board_matrix, turn)
+        self.assertEqual(actual_action, expected_action)
+
+    def test_decide_action_play_card(self):
+        # Create a board state where no cards have been played yet
+        # board_matrix = [[False]*13 for _ in range(4)]
+
+        board_matrix = [
+            [False, False, False, False, False, False, True, False,
+                False, False, False, False, False],  # Hearts
+            [False]*13,  # Diamonds
+            [False]*13,  # Clubs
+            [False]*13,  # Spades
+        ]
+
+        # Create actions for the turn
+        actions = {Action(ActionType.PLAY_CARD, Card(Suit.HEARTS, Rank.ACE)), Action(
+            ActionType.PLAY_CARD, Card(Suit.HEARTS, Rank.SIX))}
+
+        # Create a turn with the actions
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # The expected action is to play a card, because it's the only action available
+        expected_action = Action(ActionType.PLAY_CARD,
+                                 Card(Suit.HEARTS, Rank.SIX))
+
+        # Call decide_action and check that it returns the expected action
+        actual_action = ActionDecider.decide_action(board_matrix, turn)
+        self.assertEqual(actual_action, expected_action)
+
+    def test_decide_action_take_card(self):
+        # Create a board state where no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+
+        # Create actions for the turn
+        actions = {Action(ActionType.TAKE_CARD)}
+
+        # Create a turn with the actions
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # The expected action is to take a card, because it's the only action available
+        expected_action = Action(ActionType.TAKE_CARD)
+
+        # Call decide_action and check that it returns the expected action
+        actual_action = ActionDecider.decide_action(board_matrix, turn)
+        self.assertEqual(actual_action, expected_action)
+
+    def test_decide_action_pass_turn(self):
+        # Create a board state where no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+
+        # Create actions for the turn
+        actions = {Action(ActionType.PASS_TURN)}
+
+        # Create a turn with the actions
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # The expected action is to pass the turn, because it's the only action available
+        expected_action = Action(ActionType.PASS_TURN)
+
+        # Call decide_action and check that it returns the expected action
+        actual_action = ActionDecider.decide_action(board_matrix, turn)
+        self.assertEqual(actual_action, expected_action)
+
+    def test_decide_action_when_multiple_actions_are_possible(self):
+        """
+        In this test case, all action types are possible, so we check that the decide_action function correctly 
+        prioritizes actions according to the predefined priority order.
+        """
+
+        # Create a board state where no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+
+        # Define the priority for each action type
+        action_priorities = {
+            ActionType.PLAY_ALL_CARDS: 1,
+            ActionType.GIVE_CARD: 2,
+            ActionType.PLAY_CARD: 3,
+            ActionType.TAKE_CARD: 4,
+            ActionType.PASS_TURN: 5
+        }
+
+        # Create a turn with all actions
+        actions = {
+            Action(action_type) if action_type != ActionType.PLAY_CARD and action_type != ActionType.GIVE_CARD
+            else Action(action_type, Card(Suit.HEARTS, Rank.ACE))
+            for action_type in ActionType
+        }
+        turn = Turn(actions, Player(name="Alice", type=PlayerType.AI))
+
+        # Test each priority
+        for priority in range(1, 6):
+            expected_action_type = [action_type for action_type, action_priority in action_priorities.items(
+            ) if action_priority == priority][0]
+            expected_action = Action(expected_action_type) if expected_action_type != ActionType.PLAY_CARD and expected_action_type != ActionType.GIVE_CARD else Action(
+                expected_action_type, Card(Suit.HEARTS, Rank.ACE))
+
+            # Call decide_action and check that it returns the expected action
+            actual_action = ActionDecider.decide_action(board_matrix, turn)
+            self.assertEqual(actual_action, expected_action)
+
+            # Remove the action from the turn actions to check the next priority
+            turn.actions.remove(expected_action)
+
+    def test_decide_action_no_actions(self):
+        # Create a board state where no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+
+        # Create a turn with no actions
+        turn = Turn(set(), Player(name="Alice", type=PlayerType.AI))
+
+        # Call decide_action and check that it raises a ValueError
+        with self.assertRaises(ValueError):
+            ActionDecider.decide_action(board_matrix, turn)
+
+    def test_analyze_board(self):
+        board_matrix = [
+            [False, False, False, False, False, False, True, False,
+                False, False, False, False, False],  # Hearts
+            [False, False, False, False, False, True, True, True,
+                False, False, False, False, False],  # Diamonds
+            [False, False, False, False, False, False, False,
+                False, False, False, False, False, False],  # Clubs
+            [False, False, False, False, False, False, True,
+                True, True, False, False, False, False],  # Spades
+        ]
+        expected_board_state = {
+            Suit.HEARTS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.DIAMONDS: (Rank.SIX, Rank.EIGHT),
+            Suit.CLUBS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.SPADES: (Rank.SEVEN, Rank.NINE),
+        }
+        actual_board_state = ActionDecider._ActionDecider__analyze_board(
+            board_matrix)
+
+        self.assertEqual(actual_board_state, expected_board_state)
+
+    def test_analyze_board_no_cards_played(self):
+        # Test when no cards have been played yet
+        board_matrix = [[False]*13 for _ in range(4)]
+        expected_board_state = {
+            Suit.HEARTS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.DIAMONDS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.CLUBS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.SPADES: (Rank.SEVEN, Rank.SEVEN),
+        }
+        actual_board_state = ActionDecider._ActionDecider__analyze_board(
+            board_matrix)
+        self.assertEqual(actual_board_state, expected_board_state)
+
+    def test_analyze_board_all_cards_played(self):
+        # Test when all cards have been played
+        board_matrix = [[True]*13 for _ in range(4)]
+        expected_board_state = {
+            Suit.HEARTS: (Rank.ACE, Rank.KING),
+            Suit.DIAMONDS: (Rank.ACE, Rank.KING),
+            Suit.CLUBS: (Rank.ACE, Rank.KING),
+            Suit.SPADES: (Rank.ACE, Rank.KING),
+        }
+        actual_board_state = ActionDecider._ActionDecider__analyze_board(
+            board_matrix)
+        self.assertEqual(actual_board_state, expected_board_state)
+
+    def test_score_cards(self):
+        board_state = {
+            Suit.HEARTS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.DIAMONDS: (Rank.SIX, Rank.EIGHT),
+            Suit.CLUBS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.SPADES: (Rank.SEVEN, Rank.NINE),
+        }
+        cards = [
+            Card(Suit.HEARTS, Rank.EIGHT),
+            Card(Suit.DIAMONDS, Rank.FIVE),
+            Card(Suit.CLUBS, Rank.SIX),
+            Card(Suit.SPADES, Rank.TEN),
+        ]
+        expected_scores = {
+            cards[0]: 1,
+            cards[1]: 1,
+            cards[2]: 1,
+            cards[3]: 1,
+        }
+        actual_scores = ActionDecider._ActionDecider__score_cards(
+            cards, board_state)
+        self.assertEqual(actual_scores, expected_scores)
+
+    def test_score_cards_all_playable(self):
+        # Test when all cards are playable
+        board_state = {
+            Suit.HEARTS: (Rank.SIX, Rank.EIGHT),
+            Suit.DIAMONDS: (Rank.SIX, Rank.EIGHT),
+            Suit.CLUBS: (Rank.SIX, Rank.EIGHT),
+            Suit.SPADES: (Rank.SIX, Rank.EIGHT),
+        }
+        cards = [
+            Card(Suit.HEARTS, Rank.SEVEN),
+            Card(Suit.DIAMONDS, Rank.SEVEN),
+            Card(Suit.CLUBS, Rank.SEVEN),
+            Card(Suit.SPADES, Rank.SEVEN),
+        ]
+        expected_scores = {card: 0 for card in cards}  # All cards are playable
+        actual_scores = ActionDecider._ActionDecider__score_cards(
+            cards, board_state)
+        self.assertEqual(actual_scores, expected_scores)
+
+    def test_score_cards_all_unplayable(self):
+        # Test when no cards are playable
+        board_state = {
+            Suit.HEARTS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.DIAMONDS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.CLUBS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.SPADES: (Rank.SEVEN, Rank.SEVEN),
+        }
+        cards = [
+            Card(Suit.HEARTS, Rank.SIX),
+            Card(Suit.DIAMONDS, Rank.EIGHT),
+            Card(Suit.CLUBS, Rank.SIX),
+            Card(Suit.SPADES, Rank.EIGHT),
+        ]
+        # All cards are one step away from being playable
+        expected_scores = {card: 1 for card in cards}
+        actual_scores = ActionDecider._ActionDecider__score_cards(
+            cards, board_state)
+        self.assertEqual(actual_scores, expected_scores)
+
+    def test_score_cards_all_extreme(self):
+        # Test when all cards are at their furthest possible position from being playable
+        board_state = {
+            Suit.HEARTS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.DIAMONDS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.CLUBS: (Rank.SEVEN, Rank.SEVEN),
+            Suit.SPADES: (Rank.SEVEN, Rank.SEVEN),
+        }
+        cards = [
+            Card(Suit.HEARTS, Rank.ACE),
+            Card(Suit.DIAMONDS, Rank.KING),
+            Card(Suit.CLUBS, Rank.ACE),
+            Card(Suit.SPADES, Rank.KING),
+        ]
+        # All cards are six steps away from being playable
+        expected_scores = {card: 6 for card in cards}
+        actual_scores = ActionDecider._ActionDecider__score_cards(
+            cards, board_state)
+        self.assertEqual(actual_scores, expected_scores)
+
+
+if __name__ == '__main__':
+    unittest.main()
